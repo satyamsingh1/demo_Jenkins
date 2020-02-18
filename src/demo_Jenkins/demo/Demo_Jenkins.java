57edd13dad0f7cc623bbfd5de83380df56ddf858
soft.satyamkumar@gmail.com <soft.satyamkumar@gmail.com>
2020-02-18 12:42:18 +0530

    Hello Jenkins

---
+++ src/demo_Jenkins/demo/Demo_Jenkins.java
@@ -0,0 +1,13 @@
+package demo_Jenkins.demo;
+
+public class Demo_Jenkins {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("satyam.........."+i);
+		}
+	}
+
+}
